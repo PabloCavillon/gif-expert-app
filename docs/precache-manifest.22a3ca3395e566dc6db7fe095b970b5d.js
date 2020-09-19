777b877d405b4e1da0db5b827bd711fcffa68481
Pablo Cavillon <pabloa.cavillon@gmail.com>
2020-09-19 02:05:36 -0300

    docs agregado

---
+++ docs/precache-manifest.22a3ca3395e566dc6db7fe095b970b5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a5b6ff8e939bc1fc5074a68368b44e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e64963983ff7e1afaf4",
+    "url": "/static/css/main.7c41cc5b.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e64963983ff7e1afaf4",
+    "url": "/static/js/main.ddb0af8d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
